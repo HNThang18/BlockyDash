e7e463a4b2497b35d315a3cf3b3a3f151a692c38
Dang Khoi <khoind1234@gmail.com>
2025-06-19 23:40:35 +0700

    Start Screen - 2nd version

--- Assets/Scripts/StartMenuController.cs
+++ Assets/Scripts/StartMenuController.cs
@@ -5,14 +5,14 @@ public class StartMenuController : MonoBehaviour
 {
     //[SerializeField] public Screen Level1;
 
-    public void OnStartButton()
+    public void OnStartButton(string screenName)
     {
-        SceneManager.LoadScene("TestLevel");
+        SceneManager.LoadScene(screenName);
     }
 
     public void OnExitButton()
     {
-#if UNITY_EDITOR
+#if UNITY_EDITOR // to set off the debug
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
         Application.Quit();
